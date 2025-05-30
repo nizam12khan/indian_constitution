46805dd2d1fd609eb14849674da3709a37b446c5
Nizamuddin Khan <nizam12khan@gmail.com>
2025-05-30 17:09:17 +0530

    Update RSpec tests for IndianConstitution functionality

--- spec/indian_constitution_spec.rb
+++ spec/indian_constitution_spec.rb
@@ -1,11 +1,27 @@
 # frozen_string_literal: true
 
 RSpec.describe IndianConstitution do
-  it "has a version number" do
-    expect(IndianConstitution::VERSION).not_to be nil
+  describe ".get_part" do
+    it "returns the correct part by number" do
+      part = IndianConstitution.get_part("I")
+      expect(part.number).to eq("I")
+      expect(part.title).to eq("The Union and Its Territory")
+      expect(part.articles.size).to eq(4) # Part I has Articles 1-4
+    end
+
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".get_article" do
+    it "returns the correct article by number" do
+      article = IndianConstitution.get_article(1)
+      expect(article.number).to eq(1)
+      expect(article.title).to eq("Name and territory of the Union")
+      expect(article.content).to include("India, that is Bharat")
+    end
+
+    it "raises an error for an invalid article number" do
+      expect { IndianConstitution.get_article(999) }.to raise_error("Article 999 not found")
+    end
   end
+
 end
